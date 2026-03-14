f77568790a38d520635d7b39313c022ffcbf9522
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-03-14 22:51:27 +0530

    Decimal To Hexadecimal options added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,31 @@
 import java.util.*;
+class Homepage{
+    void Homepage(){
+        System.out.println(" 1) Decimal To Binary");
+        System.out.println(" 2) Decimal To Hexadecimal");
+        System.out.print("Enter Your Choice : ");
+    }
+}
 public class Main {
     static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter The Decimal Number");
-        int Deci = input.nextInt();
-        String binary = Integer.toBinaryString(Deci);
-        System.out.println("Binary ="+binary);
+        Homepage homepage = new Homepage();
+        homepage.Homepage();
+        int choice = input.nextInt();
+        switch (choice) {
+
+            case 1:
+                   System.out.println("Enter The Decimal Number");
+                   int Decimal1 = input.nextInt();
+                   String binary = Integer.toBinaryString(Decimal1);
+                   System.out.println("Binary =" + binary);
+                   break;
+            case 2:
+                System.out.print("Enter Decimal Number: ");
+                int decimal2 = input.nextInt();
+                String hexadecimal = Integer.toHexString(decimal2);
+                System.out.println("Hexadecimal = " + hexadecimal);
+                break;
+        }
     }
 }
