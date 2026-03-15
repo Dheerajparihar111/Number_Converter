ac464b8c841a69020d7f36c9abe6818f319f0b09
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-03-15 21:50:19 +0530

    added while loop and added Table format to Homepage

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import java.util.*;
 class Homepage{
     void Homepage(){
-        System.out.println(" 1) Decimal To Binary");
-        System.out.println(" 2) Decimal To Octal");
-        System.out.println(" 3) Decimal To Hexadecimal");
-        System.out.println(" 4) Binary To Decimal");
-        System.out.println(" 5) Octal To Decimal");
-        System.out.println(" 6) Hexadecimal To Decimal");
+        System.out.println("=___________________________");
+        System.out.println("| 1) Decimal To Binary      |");
+        System.out.println("| 2) Decimal To Octal       |");
+        System.out.println("| 3) Decimal To Hexadecimal |");
+        System.out.println("| 4) Binary To Decimal      |");
+        System.out.println("| 5) Octal To Decimal       |");
+        System.out.println("| 6) Hexadecimal To Decimal |");
+        System.out.println("| 7) EXIT                   |");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.print("Enter Your Choice : ");
     }
 }
@@ -14,21 +17,22 @@ public class Main {
     static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Homepage homepage = new Homepage();
+        while (true){
         homepage.Homepage();
         int choice = input.nextInt();
         switch (choice) {
 
             case 1:
-                   System.out.println("Enter The Decimal Number");
-                   int Decimal1 = input.nextInt();
-                   String binary = Integer.toBinaryString(Decimal1);
-                   System.out.println("Binary =" + binary);
-                   break;
+                System.out.println("Enter The Decimal Number");
+                int Decimal1 = input.nextInt();
+                String binary = Integer.toBinaryString(Decimal1);
+                System.out.println("Binary =" + binary);
+                break;
             case 2:
                 System.out.println("Enter The Decimal Number ");
                 int Decimal2 = input.nextInt();
                 String Octal = Integer.toOctalString(Decimal2);
-                System.out.println("Octal = "+Octal);
+                System.out.println("Octal = " + Octal);
                 break;
             case 3:
                 System.out.print("Enter Decimal Number: ");
@@ -45,16 +49,19 @@ public class Main {
             case 5:
                 System.out.println("Enter Octal Number");
                 String octal = input.next();
-                int octalToDecimal = Integer.parseInt(octal,8);
-                System.out.println("Decimal = "+octalToDecimal);
+                int octalToDecimal = Integer.parseInt(octal, 8);
+                System.out.println("Decimal = " + octalToDecimal);
                 break;
             case 6:
                 System.out.println("Enter Hexadecimal Number");
                 String hexadecimal = input.next();
-                int hexadecimalToDecimal = Integer.parseInt(hexadecimal,16);
-                System.out.println("Decimal = "+ hexadecimalToDecimal);
+                int hexadecimalToDecimal = Integer.parseInt(hexadecimal, 16);
+                System.out.println("Decimal = " + hexadecimalToDecimal);
                 break;
-
+            case 7:
+                System.out.println("Thank You For Using Number Converter ");
+                System.exit(0);
+           }
         }
     }
 }
